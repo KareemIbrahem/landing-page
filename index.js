f5f369ad90b61f14ab3253e45aa6796082b2c566
Kareem Ibrahem <kareemibrahem1999@outlook.com>
2024-01-18 16:12:02 +0200

    changes

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
   });
 
   // Form submission
-  $(".subscribe-form").on("submit click touchstart", function (event) {
+  $(".subscribe-form").on("submit touchend", function (event) {
     event.preventDefault();
 
     console.log("sheet");
